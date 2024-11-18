1a46cfed747561dc9c2187784264275680b4e3a3
Amal Saleh (ASH) <70725157+amalhq@users.noreply.github.com>
2024-11-18 03:42:37 +0300

    Delete lib directory

--- lib/widget/size_extension.dart
+++
@@ -1,7 +0,0 @@
-import 'package:car_maintenance_technicians/widget/screen_size_util.dart';
-
-extension SizeExtension on num {
-  double get sw => ScreenSizeUtil.screenWidth * this;
-
-  double get sh => ScreenSizeUtil.screenHeight * this;
-}
